e5d7f46512c05255cbf78c8b06a2768bf2cc1491
aisha dorsett <aishadorsett23@gmail.com>
2021-10-05 16:07:23 +0100

    battle updates

---
+++ spec/features/names_spec.rb
@@ -0,0 +1,11 @@
+require './app'
+
+feature 'Entering names into a form' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Charlotte vs. Mittens'
+  end
+end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -2,7 +2,7 @@ require './app'
 
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
-    visit('/')
+    visit('/test')
     expect(page).to have_content 'Testing infrastructure working!'
   end
 end
